9b38c18f78550fce42c5f91ce9812ba1dd62d79c
Peter Hellberg <peter@c7.se>
2017-04-05 12:40:29 +0200

    Added package documentation

--- microview.go
+++ microview.go
@@ -1,3 +1,45 @@
+/*
+
+Package microview is used to remote control a MicroView - OLED Arduino Module
+
+Installation
+
+    go get -u github.com/peterhellberg/microview
+
+Example
+
+    package main
+
+    import (
+    	"flag"
+    	"log"
+    	"time"
+
+    	. "github.com/peterhellberg/microview"
+    )
+
+    func main() {
+    	name := flag.String("name", "/dev/cu.usbserial-DA00SSM3", "name of the serial port")
+
+    	flag.Parse()
+
+    	mv, err := OpenMicroView(*name, Delay(90*time.Millisecond))
+    	if err != nil {
+    		log.Fatal(err)
+    	}
+
+    	mv.Run(
+    		RectFill(5, 5, 5, 15),
+    		RectFill(25, 0, 30, 15),
+    		Rect(1, 1, 20, 40),
+    		Rect(40, 20, 20, 20),
+    		Rect(40, 20, 15, 15),
+    		Rect(40, 20, 10, 10),
+    		Rect(40, 20, 5, 5),
+    	)
+    }
+
+*/
 package microview
 
 import (

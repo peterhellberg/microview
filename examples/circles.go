48d52f2b945345ca027aa89c881815e2f491c6b0
Peter Hellberg <peter@c7.se>
2017-04-05 08:39:19 +0200

    Examples on how to use the microview package

---
+++ examples/circles.go
@@ -0,0 +1,33 @@
+// +build example
+//
+// Do not build by default.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"time"
+
+	. "github.com/peterhellberg/microview"
+)
+
+func main() {
+	name := flag.String("name", "/dev/cu.usbserial-DA00SSM3", "name of the serial port")
+
+	flag.Parse()
+
+	mv, err := NewMicroView(*name, Delay(90*time.Millisecond))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	mv.Run(
+		CircleFill(5, 5, 5),
+		Circle(1, 1, 20),
+		Circle(40, 20, 20),
+		Circle(40, 20, 15),
+		Circle(40, 20, 10),
+		Circle(40, 20, 5),
+	)
+}
---
+++ examples/text.go
@@ -0,0 +1,29 @@
+// +build example
+//
+// Do not build by default.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"strings"
+
+	. "github.com/peterhellberg/microview"
+)
+
+func main() {
+	name := flag.String("name", "/dev/cu.usbserial-DA00SSM3", "name of the serial port")
+	text := flag.String("text", "Hello From Go!", "text to display on the MicroView")
+
+	flag.Parse()
+
+	mv, err := NewMicroView(*name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i, s := range strings.Split(*text, " ") {
+		mv.DrawString(0, uint8(10*i), s)
+	}
+}

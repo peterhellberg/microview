4a3101b4927281d1d7e200eb9f1bd9ed723f8dc5
Peter Hellberg <peter@c7.se>
2017-04-05 16:59:05 +0200

    Test NewMicroView

--- microview_test.go
+++ microview_test.go
@@ -2,8 +2,31 @@ package microview
 
 import (
 	"bytes"
+	"image"
+	"io"
+	"io/ioutil"
 	"testing"
 )
+
+func TestNewMicroView(t *testing.T) {
+	buf := bytes.NewBufferString("")
+
+	mv := NewMicroView(struct {
+		io.ReadWriter
+		io.Closer
+	}{buf, ioutil.NopCloser(nil)}, Delay(0))
+	defer mv.Close()
+
+	if got, want := mv.Bounds(), image.Rect(0, 0, 64, 48); !got.Eq(want) {
+		t.Fatalf("m.Bounds() = %v, want %v", got, want)
+	}
+
+	mv.Run(Rect(5, 10, 15, 20))
+
+	if got, want := buf.String(), "9,5,10,15,20"; got != want {
+		t.Fatalf("buf.String() = %q, want %q", got, want)
+	}
+}
 
 func TestClear(t *testing.T) {
 	for _, tt := range []struct {
